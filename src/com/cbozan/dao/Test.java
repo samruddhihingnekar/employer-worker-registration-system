1ffd6fe506e51e70017cc26bbb8ca3cd1a34108d
samruddhihingnekar <hingnekarsamruddhi@gmail.com>
2025-12-10 23:39:49 +0530

    Refactored the code as per comments

--- src/com/cbozan/dao/Test.java
+++ src/com/cbozan/dao/Test.java
@@ -1,16 +1,19 @@
 package com.cbozan.dao;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ArrayList<Person> list = new ArrayList<Person>();
-		list.add(new Person(101, "Vishal", 2000, "Mumbai"));
-		list.add(new Person(102, "Aniket", 3000, "Pune"));
-		list.add(new Person(103, "Rohit", 4000, "Nagpur"));
+		List<Person> student = new ArrayList<Person>();
+		student.add(new Person(101, "Vishal", 2000, "Mumbai"));
+		student.add(new Person(102, "Aniket", 3000, "Pune"));
+		student.add(new Person(103, "Rohit", 4000, "Nagpur"));
 		
-		System.out.println(list); 
+		for(Person stu :student) {
+			System.out.println(stu);
+		}
 		
 	}
 
