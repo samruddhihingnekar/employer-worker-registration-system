e2121a1c3ae7659a3a984aa350c3b6ba18cf72bb
samruddhihingnekar <hingnekarsamruddhi@gmail.com>
2025-12-10 17:36:33 +0530

    Added Person class

---
+++ src/com/cbozan/dao/Person.java
@@ -0,0 +1,51 @@
+package com.cbozan.dao;
+
+public class Person {
+
+	private int id;
+	private String name;
+	private double salary;
+	private String address;
+	
+	public Person() {
+		
+	}
+	public Person(int id, String name, double salary, String address) {
+		
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.address = address;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", salary=" + salary + ", address=" + address + "]";
+	}
+	
+	
+}
